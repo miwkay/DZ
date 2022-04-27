da25302209123a473f59253296473125a682e7bd
Miwkay <jedy3666@gmail.com>
2022-04-27 13:13:40 +0300

    Corrected dz4.py

--- dz4.py
+++ dz4.py
@@ -4,21 +4,31 @@ import datetime
 class Person:
     person_age = 18
 
+    def __init__(self, age):
+        self.person_age = age
+
+    def __str__(self):
+        return (f"Age: {self.valid_age(self.person_age)} "
+                f"Year of birh: {self.year_of_birth(self.person_age)}")
+
     @classmethod
-    def valid_age(cls, arg):
-        if cls.person_age <= arg:
+    def valid_age(cls, age):
+        if cls.person_age <= age:
             print('Adult')
         else:
             print('Too young')
 
-    def __init__(self, age):
-        self.valid_age = age
-
     @staticmethod
     def year_of_birth(age):
-        year_now = datetime.datetime.now()
-        print('Year of birh:', year_now.year - age)
+        date_now = datetime.datetime.now()
+        year_birth = date_now.year - age
+        print('Year of birh:', year_birth)
 
 
-print(Person.valid_age(15))  # Output: Too young
-print(Person.year_of_birth(15))  # Output: Year of birth: 2007
+person1 = Person(29)
+print(person1)  # Output: Adult Year of birth: 1993
+person2 = Person(17)
+print(person2)  # Output: Too young Year of birth: 2005
+
+print(Person.valid_age(29))  # Output: Adult
+print(Person.year_of_birth(17))  # Output: Year of birth: 2005
